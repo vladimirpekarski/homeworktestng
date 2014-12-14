30d6261b4c93f1efb4d682244ff767f8b806ad22
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-14 20:32:35 +0300

    StringReverseOrder modification

--- src/classes/StringReverseOrder.java
+++ src/classes/StringReverseOrder.java
@@ -1,31 +1,19 @@
 package classes;
 
 public class StringReverseOrder implements CharSequence {
-    private String originalValue;
     private String reversedValue;
 
-    public StringReverseOrder(String originalValue) {
-        this.originalValue = originalValue;
-        reverseValue();
-    }
 
-    public StringReverseOrder() {
-        this("Default value. Default constructor");
-    }
-
-    public String getReversedValue() {
-        return reversedValue;
-    }
-
-    private void reverseValue () {
+    public String reverseValue (String originalValue) {
         char [] tempCharArray = new char[originalValue.length()];
         char [] charArray = originalValue.toCharArray();
 
         for (int i = 0; i < charArray.length; i++) {
             tempCharArray[i] = charArray[charArray.length - 1 - i];
         }
+        reversedValue =  String.valueOf(tempCharArray)        ;
 
-        reversedValue = String.valueOf(tempCharArray);
+        return reversedValue;
     }
 
     @Override
