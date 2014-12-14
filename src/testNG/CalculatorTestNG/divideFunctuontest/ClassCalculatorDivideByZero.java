32d604406a8ba2df84bbcedfde9fabe8c7a1663e
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-14 17:14:22 +0300

    ClassCalculatorMultiplyFunctionTest2

---
+++ src/testNG/CalculatorTestNG/divideFunctuontest/ClassCalculatorDivideByZero.java
@@ -0,0 +1,19 @@
+package testNG.CalculatorTestNG.divideFunctuontest;
+
+import classes.Calculator;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ClassCalculatorDivideByZero {
+    private Calculator calculator;
+
+    @BeforeTest
+    public void initialize() {
+        calculator = new Calculator();
+    }
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void test() {
+       calculator.divide(5.0, 0.0);
+    }
+}
