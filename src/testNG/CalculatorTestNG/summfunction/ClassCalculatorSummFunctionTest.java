81b114d964e0eee02efd8af118468d368bda0460
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-14 15:20:15 +0300

    ClassCalculatorSummFunctionTest

---
+++ src/testNG/CalculatorTestNG/summfunction/ClassCalculatorSummFunctionTest.java
@@ -0,0 +1,28 @@
+package testNG.CalculatorTestNG.summfunction;
+
+import classes.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ClassCalculatorSummFunctionTest {
+    private Calculator calculator;
+    private static Object[][] testingDataForFunctSumm;
+
+    @BeforeTest
+    public void initialize() {
+        calculator = new Calculator();
+        testingDataForFunctSumm  = DataFromFileSummFunct.readFromFile("./resources/dataForTestClassCalcFunctSumm.yaml");
+    }
+
+    @DataProvider(name = "test2")
+    public static Object[][] classCalcFunctSumm() {
+        return testingDataForFunctSumm;
+    }
+
+    @Test(dataProvider = "test2")
+    public void TestForClassCalcFunctSumm(Double firstValue, Double secondValue, Double expectedResult) {
+        Assert.assertEquals(calculator.summ(firstValue, secondValue), expectedResult);
+    }
+}
