9a05808962a0e289551382161ffc0534b4f53e9a
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-21 22:47:22 +0300

    Addong Log4j

--- src/test/java/testNG/PrimeNumberCheckerTestNG/simpletests/PrimeNumberCheckerSimpleTest.java
+++ src/test/java/testNG/PrimeNumberCheckerTestNG/simpletests/PrimeNumberCheckerSimpleTest.java
@@ -1,6 +1,8 @@
 package testNG.PrimeNumberCheckerTestNG.simpletests;
 
 import classes.PrimeNumberChecker;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -8,25 +10,52 @@ import org.testng.annotations.Test;
 
 public class PrimeNumberCheckerSimpleTest {
     private PrimeNumberChecker primeNumberChecker;
+    private static final Logger LOG = Logger.getLogger(PrimeNumberCheckerSimpleTest.class);
 
     @BeforeTest
     public void initialize() {
         primeNumberChecker = new PrimeNumberChecker();
+        PropertyConfigurator.configure("./src/log4j.properties");
+        LOG.info("Initialize finished");
     }
 
     @Test
     public void testPrimeNumberCheckerTrue() {
-        Assert.assertTrue(primeNumberChecker.validate(2));
+        try {
+            LOG.info("Test Starts");
+            LOG.debug("Value to test: 2");
+            Assert.assertTrue(primeNumberChecker.validate(2));
+            LOG.info("Test Ends");
+        } catch (AssertionError e) {
+            LOG.error("testPrimeNumberCheckerTrue test fails" + e);
+            Assert.fail("testPrimeNumberCheckerTrue test fails");
+        }
     }
 
     @Test
     public void testPrimeNumberCheckerFailse() {
-        Assert.assertFalse(primeNumberChecker.validate(6));
+        try {
+            LOG.info("Test Starts");
+            LOG.debug("Value to test: 6");
+            Assert.assertFalse(primeNumberChecker.validate(6));
+            LOG.info("Test Ends");
+        } catch (AssertionError e) {
+            LOG.error("testPrimeNumberCheckerFailse test fails" + e);
+            Assert.fail("testPrimeNumberCheckerFailse test fails");
+        }
     }
 
     @Test(expectedExceptions = NullPointerException.class)
     public void testNullPointerException() {
-        primeNumberChecker.validate(null);
+        try {
+            LOG.info("Test Starts");
+            LOG.debug("Value to test: null");
+            primeNumberChecker.validate(null);
+            LOG.info("Test Ends");
+        } catch (AssertionError e) {
+            LOG.error("testNullPointerException test fails" + e);
+            Assert.fail("testNullPointerException test fails");
+        }
     }
 
     @Test
