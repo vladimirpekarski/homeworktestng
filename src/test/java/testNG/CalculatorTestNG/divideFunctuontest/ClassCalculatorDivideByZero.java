4e4abeaa005c657fd77ec4253e8b246835e9764c
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-21 16:01:34 +0300

    Adding Log4j

--- src/test/java/testNG/CalculatorTestNG/divideFunctuontest/ClassCalculatorDivideByZero.java
+++ src/test/java/testNG/CalculatorTestNG/divideFunctuontest/ClassCalculatorDivideByZero.java
@@ -1,20 +1,27 @@
 package testNG.CalculatorTestNG.divideFunctuontest;
 
 import classes.Calculator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class ClassCalculatorDivideByZero {
 
+public class ClassCalculatorDivideByZero {
+    private static final Logger LOG = Logger.getLogger(ClassCalculatorDivideByZero.class);
     private Calculator calculator;
+
 
     @BeforeTest
     public void initialize() {
         calculator = new Calculator();
+        PropertyConfigurator.configure("./src/log4j.properties");
     }
+
 
     @Test(expectedExceptions = ArithmeticException.class)
     public void divideByZeroTest() {
+       LOG.info("Start Test");
        calculator.divide(5.0, 0.0);
     }
 }
