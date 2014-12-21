0467ab8a8b101d8fa409d8d8a65bd4e901e4df42
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-21 20:08:18 +0300

    adding new LOG

--- src/test/java/testNG/CalculatorTestNG/multiplyFunctionTest/ClassCalculatorMultiplyFunctionTest.java
+++ src/test/java/testNG/CalculatorTestNG/multiplyFunctionTest/ClassCalculatorMultiplyFunctionTest.java
@@ -1,6 +1,7 @@
 package testNG.CalculatorTestNG.multiplyFunctionTest;
 
 import classes.Calculator;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -8,6 +9,7 @@ import org.testng.annotations.Test;
 import testNG.dataprovideclasses.DataFromFileToCalculator;
 
 public class ClassCalculatorMultiplyFunctionTest {
+    private static final Logger LOG = Logger.getLogger(ClassCalculatorMultiplyFunctionTest.class);
     private Calculator calculator;
     private static Object[][] testingDataForFunctSumm;
 
@@ -15,15 +17,21 @@ public class ClassCalculatorMultiplyFunctionTest {
     public void initialize() {
         calculator = new Calculator();
         testingDataForFunctSumm  = DataFromFileToCalculator.readFromFile("./src/main/resources/dataForTestClassCalcFunctMultiply.yaml");
+        LOG.info("Initialize finished");
     }
 
     @DataProvider(name = "test4")
     public static Object[][] classCalcFunctMultiply() {
+        LOG.info("Dataprovider starts");
+
         return testingDataForFunctSumm;
     }
 
     @Test(dataProvider = "test4")
     public void TestForClassCalcFunctMultiply(Double firstValue, Double secondValue, Double expectedResult) {
+        LOG.info("Test Starts");
+        LOG.debug("First value: " + firstValue + " Second value: " + secondValue + " Expected result: " + expectedResult);
         Assert.assertEquals(calculator.multiply(firstValue, secondValue), expectedResult);
+        LOG.info("Test Ends");
     }
 }
