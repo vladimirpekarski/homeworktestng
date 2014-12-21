1aed62c997fefb116b0c427d39f5f54363751e0b
vladimirpekarski <vladimir.pekarski@gmail.com>
2014-12-21 20:08:18 +0300

    adding new LOG

--- src/test/java/testNG/CalculatorTestNG/multiplyFunctionTest/ClassCalculatorMultiplyFunctionTest.java
+++ src/test/java/testNG/CalculatorTestNG/multiplyFunctionTest/ClassCalculatorMultiplyFunctionTest.java
@@ -1,6 +1,8 @@
 package testNG.CalculatorTestNG.multiplyFunctionTest;
 
 import classes.Calculator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -8,6 +10,7 @@ import org.testng.annotations.Test;
 import testNG.dataprovideclasses.DataFromFileToCalculator;
 
 public class ClassCalculatorMultiplyFunctionTest {
+    private static final Logger LOG = Logger.getLogger(ClassCalculatorMultiplyFunctionTest.class);
     private Calculator calculator;
     private static Object[][] testingDataForFunctSumm;
 
@@ -15,15 +18,22 @@ public class ClassCalculatorMultiplyFunctionTest {
     public void initialize() {
         calculator = new Calculator();
         testingDataForFunctSumm  = DataFromFileToCalculator.readFromFile("./src/main/resources/dataForTestClassCalcFunctMultiply.yaml");
+        PropertyConfigurator.configure("./src/log4j.properties");
+        LOG.info("Initialize finished");
     }
 
     @DataProvider(name = "test4")
     public static Object[][] classCalcFunctMultiply() {
+        LOG.info("Dataprovider starts");
+
         return testingDataForFunctSumm;
     }
 
     @Test(dataProvider = "test4")
     public void TestForClassCalcFunctMultiply(Double firstValue, Double secondValue, Double expectedResult) {
+        LOG.info("Test Starts");
+        LOG.debug("First value: " + firstValue + " Second value: " + secondValue + " Expected result: " + expectedResult);
         Assert.assertEquals(calculator.multiply(firstValue, secondValue), expectedResult);
+        LOG.info("Test Ends");
     }
 }
--- src/test/java/testNG/CalculatorTestNG/sqrtFunctionTest/ClassCalcSqrtNegativeValue.java
+++ src/test/java/testNG/CalculatorTestNG/sqrtFunctionTest/ClassCalcSqrtNegativeValue.java
@@ -1,20 +1,27 @@
 package testNG.CalculatorTestNG.sqrtFunctionTest;
 
 import classes.Calculator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class ClassCalcSqrtNegativeValue {
+    private static final Logger LOG = Logger.getLogger(ClassCalcSqrtNegativeValue.class);
     private Calculator calculator;
 
     @BeforeTest
     public void initialize() {
         calculator = new Calculator();
+        PropertyConfigurator.configure("./src/log4j.properties");
+        LOG.info("initialize finished");
     }
 
     @Test
     public void negativeValueInSqrt() {
+        LOG.info("Test Starts");
         Assert.assertTrue(Double.isNaN(calculator.sqrt(-5)));
+        LOG.info("Test Ends");
     }
 }
